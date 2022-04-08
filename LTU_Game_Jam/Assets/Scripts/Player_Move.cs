042ed2453d2cb4307eee1613da3d96ec86e880ab
csjbennett <60185886+csjbennett@users.noreply.github.com>
2022-04-08 19:12:49 -0400

    Started work on movement script

--- LTU_Game_Jam/Assets/Scripts/Player_Move.cs
+++ LTU_Game_Jam/Assets/Scripts/Player_Move.cs
@@ -5,17 +5,36 @@ using UnityEngine;
 public class Player_Move : MonoBehaviour
 {
     public CharacterController ctrl;
+    public GameObject cam;
+    public Camera mainCam;
 
+    public float walkSpeed;
+    public float lookSensitivity;
+
+    private float lookElevation = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float hor = Input.GetAxis("Horizontal") * Time.deltaTime * walkSpeed;
+        float ver = Input.GetAxis("Vertical") * Time.deltaTime * walkSpeed;
+        float azi = Input.GetAxis("Mouse X") * lookSensitivity;
+        float ele = Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        Vector3 move = new Vector3 (hor, 0f, ver);
+
+        lookElevation = Mathf.Clamp(lookElevation -= ele, -89f, 89f);
+
+        ctrl.Move(transform.forward * ver + transform.right * hor);
+
+        cam.transform.rotation = Quaternion.Euler(lookElevation * lookSensitivity, 0, 0);
+
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + (azi * lookSensitivity), 0);
     }
 }
