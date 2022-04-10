32c5e5fda2fa66a73b2631a7e322a33779d3b41c
csjbennett <60185886+csjbennett@users.noreply.github.com>
2022-04-10 01:03:33 -0400

    Eye men added to Anxiety

--- LTU_Game_Jam/Assets/Scripts/Eye_Move.cs
+++ LTU_Game_Jam/Assets/Scripts/Eye_Move.cs
@@ -17,6 +17,6 @@ public class Eye_Move : MonoBehaviour
         // Rotates the eye toward the player from any angle (borrowed from NPCLook)
         var targetRotation = Quaternion.LookRotation((player.position + Vector3.up * 0.25f) - eye.position);
         // Smoothly rotate towards the target point.
-        eye.rotation = Quaternion.Slerp(eye.rotation, targetRotation * Quaternion.Euler(Vector3.up * -90), Time.deltaTime * 3f);
+        eye.rotation = Quaternion.Slerp(eye.rotation, targetRotation * Quaternion.Euler(Vector3.up * -90), Time.deltaTime * 15f);
     }
 }
